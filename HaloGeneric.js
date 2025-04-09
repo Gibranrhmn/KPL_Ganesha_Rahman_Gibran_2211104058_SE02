ab13dbeb5372857ff39b6e8039bbbfe90bb5a06b
Ganesha Rahman Gibran <136820158+Gibranrhmn@users.noreply.github.com>
2025-04-09 23:00:37 +0700

    push generic method

---
+++ HaloGeneric.js
@@ -0,0 +1,11 @@
+class HaloGeneric {
+    SapaUser(x) {
+      console.log(`Halo user ${x}`);
+    }
+  }
+  
+  const halo = new HaloGeneric();
+  
+  halo.SapaUser("Ganesha Gibran");
+  
+  console.log("=== Code Execution Successful ===");
